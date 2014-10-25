621340132d0ab40b8ab5be6a49d1d53f4a463bce
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-25 10:21:11 -0400

    done calc

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-var button = document.getElementById('calculateBtn');
+var calBtn = document.getElementById('calculateBtn');
+var addBtn = document.getElementById('button1');
+var subBtn = document.getElementById('button2');
+var mltBtn = document.getElementById('button3');
+var divBtn = document.getElementById('button4');
 var num1 = document.getElementById('num_one');
 var num2 = document.getElementById('num_two');
+var ans = document.getElementById('answer');
+var operation=document.getElementById('sign');
+var solution;
 
-button.onclick = function() {
+addBtn.onclick = function() {
+  operation.innerHTML="+";
+};
 
 
 
-  var solution = parseInt(num1.value) + parseInt(num2.value);
-  var ans = document.getElementById('answer');
+subBtn.onclick = function() {
+  operation.innerHTML="-";
+};
+
+mltBtn.onclick = function() {
+  operation.innerHTML="*";
+};
+
+divBtn.onclick = function() {
+  operation.innerHTML="/";
+};
+
+
+
+calBtn.onclick = function() {
+  if(operation.innerHTML==="+"){
+  solution = parseInt(num1.value) + parseInt(num2.value);
   ans.innerHTML = solution;
+  }
 
+  if(operation.innerHTML==="-"){
+  solution = parseInt(num1.value) - parseInt(num2.value);
+  ans.innerHTML = solution;
+  }
 
+  if(operation.innerHTML==="*"){
+  solution = parseInt(num1.value) * parseInt(num2.value);
+  ans.innerHTML = solution;
+  }
 
-}
+  if(operation.innerHTML==="/"){
+  solution = parseInt(num1.value) / parseInt(num2.value);
+  ans.innerHTML = solution;
+  }
+
+};
